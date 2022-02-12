dbd27f0a3dffcd272d9265a6dd8ba7a4931fc2cb
jrtknauer <jrtknauer@gmail.com>
2022-02-12 13:08:21 -0700

    Replaced SECRET_KEY with environment variable.
    Signed-off-by: jrtknauer <jrtknauer@gmail.com>

--- socialdisto/settings.py
+++ socialdisto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import django_on_heroku
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=7jjv=iiv)yalp#fe!ara%8qwwc#jrfu-u**o)(b0&hn-gnk70'
+SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
