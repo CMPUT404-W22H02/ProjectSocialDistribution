25f9628bb71a6163d391f40ca18717499282d6b3
jrtknauer <jrtknauer@gmail.com>
2022-03-04 10:49:18 -0700

    Reapplying admin urls for Heroku deployment
    Signed-off-by: jrtknauer <jrtknauer@gmail.com>

--- socialdisto/urls.py
+++ socialdisto/urls.py
@@ -33,7 +33,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('inbox/', include('inbox.urls'))
 ]
