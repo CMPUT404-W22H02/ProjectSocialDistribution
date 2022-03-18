34373b9fab9d276d09ab39a75b6b48d058b9fb01
ljzhang@ualberta.ca <ljzhang123@users.noreply.github.com>
2022-03-18 03:48:20 -0600

    homepage with only navbar added to path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/login"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
+import HomePage from "./pages/homePage";
 
 function App() {
+  // Specify paths here
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/login"><Login /></Route>
-        </Routes>
-      </BrowserRouter>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login"/>}/> 
+        <Route path="/login" element={<Login />}/>
+        <Route path="/home" element={<HomePage />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
