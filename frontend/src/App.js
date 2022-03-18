01b06ff533bd3aa48e17429d8a1f27e74a58b1c6
ljzhang@ualberta.ca <ljzhang123@users.noreply.github.com>
2022-03-17 22:29:49 -0600

    added route to login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/login"><Login /></Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +24,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ frontend/src/pages/login/Login.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { Button, Input } from "@chakra-ui/react";
+
+function Login() {
+  return (
+    console.log("hello world")
+  );
+}
+
+export default Login;
+
