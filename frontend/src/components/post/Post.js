f6da3cd4ca338bcde28f4f72da1a6197061c6249
ljzhang@ualberta.ca <ljzhang123@users.noreply.github.com>
2022-03-19 01:02:14 -0600

    further polish of post component with the addition of collapsible comment box

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,41 +1,70 @@
+import { useEffect, useState } from "react";
 import {
-    Flex,
-    Heading,
-    Button,
-    Stack,
-    Avatar,
-    HStack,
-    useDisclosure,
+  Flex,
+  Heading,
+  Button,
+  IconButton,
+  Stack,
+  Box,
+  Avatar,
+  HStack,
+  useDisclosure,
+  Collapse,
+  Container,
+  Input,
+  Text,
+  ButtonGroup
 } from "@chakra-ui/react";
-import { FaCommentAlt, FaThumbsUp } from "react-icons/fa";
+import { FaComment, FaThumbsUp } from "react-icons/fa";
 import EditDialog from "../editDialog";
 
 function Post() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen: isEditOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen: isCommentOpen, onToggle } = useDisclosure();
+  // const [showComment, setShowComment] = useState(false);
+
+  // function handleCommentClick() {
+  //   setShowComment(!showComment);
+  // }
 
   return (
-    <Flex width="50rem" height="25rem" boxShadow="lg" py="2" alignContent="center">
-      <Stack direction="column" spacing="4" px="4" justify="space-between">
-        <HStack paddingTop="4" spacing="3">
+    <Flex width="50rem" minH="10rem" boxShadow="lg" py="2" alignContent="center" flexDirection="column">
+      <Stack direction="column" spacing="3" px="4" justify="space-between">
+        <HStack pt="4" ml="2" spacing="3">
           <Avatar/>
           <Heading size="md">John Doe</Heading>
         </HStack>
-        <Flex minH="10" minW="48rem">
-          Post body here
-        </Flex>
+        <Container fontWeight="medium" pt="4">
+          Title
+        </Container>
+        <Container minH="10" minW="48rem">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In faucibus tempor purus sit amet viverra. 
+          Aenean non fringilla sem. Sed rhoncus eros lorem, vitae maximus dui laoreet sed. Ut mollis eleifend eros vitae faucibus. 
+          Praesent id viverra velit. Praesent tempor tempor erat, nec placerat orci consectetur quis. 
+          Mauris lorem lacus, sagittis nec sollicitudin nec, congue et diam. Vivamus nec nisi consequat, feugiat tortor a, porta odio. 
+          Aliquam erat volutpat. Suspendisse potenti. Donec ac luctus nunc, at elementum justo. 
+          Ut nisi mauris, pharetra pellentesque elementum nec, feugiat eu ante. Duis erat tellus, viverra in odio at, viverra dictum odio. 
+          Nunc lobortis sapien non arcu consequat, sit amet vestibulum purus dignissim. 
+          Vivamus eros arcu, eleifend vel ornare id, blandit sit amet tortor.
+        </Container>
         <HStack justify="space-between">
-          <Flex>
-            <Button variant="link">
-              <FaThumbsUp/>
+          <ButtonGroup isAttached>
+            <Button leftIcon={<FaThumbsUp/>} variant="ghost">
+              9 Likes
             </Button>
-            <Button variant="link">
-              <FaCommentAlt/>
+            <Button leftIcon={<FaComment/>} variant="ghost" onClick={onToggle}>
+              4 Comments
             </Button>
-          </Flex>
-          <Button variant="solid" onClick={onOpen}>Edit</Button>
-          <EditDialog isOpen={isOpen} onClose={onClose}/>
+          </ButtonGroup>
+          <Button variant="solid" onClick={onOpen} right="0">Edit</Button>
+          <EditDialog isOpen={isEditOpen} onClose={onClose}/>
         </HStack>
       </Stack>
+      <Collapse in={isCommentOpen} animateOpacity>
+        <Box my="2" mx="4">
+          <Input placeholder="Write a comment"/>
+        </Box>
+      </Collapse>
     </Flex>
   );
 }
