54278ba957b8e9448d3a70a1cf4b03a3a09e7e4d
jackie174 <pcheng1@ualberta.ca>
2022-03-30 15:21:34 -0600

    Update EditDialog.js

--- frontend/src/components/editDialog/EditDialog.js
+++ frontend/src/components/editDialog/EditDialog.js
@@ -11,11 +11,21 @@ import {
   Input,
   FormControl,
   Button,
+  Stack,
+  Checkbox
 } from '@chakra-ui/react';
-
+import { Radio, RadioGroup } from '@chakra-ui/react'
 function EditDialog({ post, isOpen, onClose }) {
-  // const [title, setTitle] = useState(post.title);
-  // const [desc, setDesc] = useState(post.description);
+  console.log(post)
+  // once they pass post data then you just set post.title in each useState
+  //example: const [title, setTitle] = useState(post.title);
+  //post.title is get data after we click post, then it will justdisplay tehm in the input filed
+  const [title, setTitle] = useState();
+  const [description, setDesc] = useState();
+  const [content, setContent] = useState();
+  const [categories, setCategories] = useState();
+  const [unlisted, setUnlisted] = useState();
+  const [visibility, setVisibility] = useState();
 
   return (
     <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
@@ -27,13 +37,64 @@ function EditDialog({ post, isOpen, onClose }) {
         <ModalBody>
           <FormControl>
             <FormLabel>Title</FormLabel>
-            <Input/>
+            <Input
+            placeholder="Title"
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+            value={title}
+            onChange={(e)=>setTitle(e.target.value)}
+        />
           </FormControl>
           
           <FormControl>
             <FormLabel>Description</FormLabel>
-            <Input/>
+            <Input
+            placeholder="Description"
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+            value={description}
+            onChange={(e)=>setDesc(e.target.value)}
+        />
           </FormControl>
+          <FormControl>
+            <FormLabel>Content</FormLabel>
+            <Input
+            placeholder="Content"
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+            value={content}
+            onChange={(e)=>setContent(e.target.value)}
+        />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Categories</FormLabel>
+            <Input
+            placeholder="Categories"
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+            value={categories}
+            onChange={(e)=>setCategories(e.target.value)}
+        />
+          </FormControl>
+          
+          <FormControl>
+            <FormLabel>Visibility</FormLabel>
+              <RadioGroup onChange={setVisibility} value={visibility}>
+                <Stack direction='row'>
+                  <Radio visibility="PUBLIC">PUBLIC</Radio>
+                  <Radio visibility="FRIENDS" >FRIENDS</Radio>
+                </Stack>
+              </RadioGroup>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Unlisted</FormLabel>
+            <Stack spacing={5} direction='row'>
+              <Checkbox defaultChecked isChecked ={unlisted}  onChange={(e)=>setUnlisted(e.target.checked)}>
+              Unlisted
+              </Checkbox>
+            </Stack>
+          </FormControl>
+
         </ModalBody>
 
         <ModalFooter>
